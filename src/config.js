56cfec3b2e9aff188fc65bc163726b23ecf6fa91
khinji032025 <khenjeboligao79@gmail.com>
2026-05-12 07:20:14 +0800

    fix cors proxy

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const API_BASE = 'https://kenjiscafe.infinityfreeapp.com/api'
+const API_BASE = 'https://corsproxy.io/?https://kenjiscafe.infinityfreeapp.com/api'
 const IMG_BASE = 'https://kenjiscafe.infinityfreeapp.com/images'
 
 export { API_BASE, IMG_BASE }
