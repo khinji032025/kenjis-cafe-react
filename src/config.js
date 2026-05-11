f59d69a6fde12dba3fe26254aba53ef5c2806e60
khinji032025 <khenjeboligao79@gmail.com>
2026-05-12 07:26:47 +0800

    revert to direct url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const API_BASE = 'https://api.allorigins.win/raw?url=https://kenjiscafe.infinityfreeapp.com/api'
+const API_BASE = 'https://kenjiscafe.infinityfreeapp.com/api'
 const IMG_BASE = 'https://kenjiscafe.infinityfreeapp.com/images'
 
 export { API_BASE, IMG_BASE }
